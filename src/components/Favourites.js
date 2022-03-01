cd6b78195f128484a6790ca6c3f437b0661c57f1
Sahilbalhra <sahilbalhra0@gmail.com>
2022-03-01 20:13:06 +0530

    some changes

--- src/components/Favourites.js
+++ src/components/Favourites.js
@@ -3,12 +3,13 @@ import Pagination from "./Pagination";
 
 const Favourites = () => {
   const [curGenre, setCurGenre] = useState("All Genres");
+  //table content
   return (
     <>
       <div className="mt-4 px-2 flex justify-center flex-wrap space-x-2 ">
         <button
           className={
-            curGenre == "All Genres"
+            curGenre === "All Genres"
               ? "m-2 text-lg p-1 px-2 bg-blue-400 text-white rounded-xl font-bold "
               : "m-2 hover:bg-blue-400 text-lg p-1 px-2 bg-white text-black rounded-xl font-bold "
           }
@@ -17,7 +18,7 @@ const Favourites = () => {
         </button>
         <button
           className={
-            curGenre == "Action"
+            curGenre === "Action"
               ? "m-2 text-lg p-1 px-2 bg-blue-400 text-white rounded-xl font-bold "
               : "m-2 hover:bg-blue-400 text-lg p-1 px-2 bg-white text-black rounded-xl font-bold "
           }
@@ -25,6 +26,7 @@ const Favourites = () => {
           Action
         </button>
       </div>
+      {/* input section */}
       <div className="text-center">
         <input
           type="text"
@@ -36,6 +38,95 @@ const Favourites = () => {
           className="border border-3 text-center p-1 m-2 text-black rounded-xl"
           placeholder="Rows"
         />
+      </div>
+      {/* table */}
+      <div className="flex justify-center">
+        <div className="flex flex-col">
+          <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+              <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <table className="min-w-full divide-y divide-gray-200">
+                  <thead className="bg-gray-50">
+                    <tr>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Name
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        <div className="flex">
+                          <img
+                            src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png"
+                            className="mr-2 cursor-pointer"
+                            alt=""
+                            // onClick={() => {
+                            //   setPopularity(0);
+                            //   setRating(-1);
+                            // }}
+                          />
+                          Rating
+                          <img
+                            src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-down-arrows-those-icons-lineal-those-icons-4.png"
+                            // onClick={() => {
+                            //   setPopularity(0);
+                            //   setRating(1);
+                            // }}
+                            alt=""
+                            className="ml-2 mr-2 cursor-pointer"
+                          />
+                        </div>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        <div className="flex">
+                        <img
+                            src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-up-arrows-those-icons-lineal-those-icons-3.png"
+                            className="mr-2 cursor-pointer"
+                            alt=""
+                            // onClick={() => {
+                            //   setPopularity(0);
+                            //   setRating(-1);
+                            // }}
+                          />          
+                          Popularity
+                          <img
+                            src="https://img.icons8.com/external-those-icons-lineal-those-icons/24/000000/external-down-arrows-those-icons-lineal-those-icons-4.png"
+                            // onClick={() => {
+                            //   setPopularity(0);
+                            //   setRating(1);
+                            // }}
+                            alt=""
+                            className="ml-2 mr-2 cursor-pointer"
+                          />
+                          
+                        </div>
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Genre
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                      >
+                        Remove
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className="bg-white divide-y divide-gray-200"></tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <Pagination />
     </>
